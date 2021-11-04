e6bf978c9d7aca04932727702a514c94c9bdb0b0
FrontLeejonghun <leejonghun32425@gmail.com>
2021-11-04 16:07:39 +0900

    Update - at-rule-no-unknown null로 변경

--- stylelint.config.js
+++ stylelint.config.js
@@ -5,7 +5,7 @@ module.exports = {
   rules: {
     'color-hex-case': 'lower',
     'comment-empty-line-before': 'always',
-    'at-rule-no-unknown': false,
+    'at-rule-no-unknown': null,
     'declaration-block-trailing-semicolon': 'always',
     'number-leading-zero': 'always',
     'rule-empty-line-before': 'always-multi-line',
