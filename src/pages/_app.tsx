e29f59f1463ac4f9b5f4931ae2c0a5f07aa2d04d
FrontLeejonghun <leejonghun32425@gmail.com>
2021-11-16 09:15:48 +0900

    Delete - recoil debuger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
 import { RecoilRoot } from 'recoil';
 
 import 'styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  const [root, setRoot] = useState<HTMLElement>();
-
-  const RecoilizeDebugger = dynamic(
-    () => {
-      return import('react-recoilize');
-    },
-    { ssr: false },
-  );
-
-  useEffect(() => {
-    if (typeof window.document !== 'undefined') {
-      setRoot(document.getElementById('__next') as HTMLElement);
-    }
-  }, [root]);
   return (
     <RecoilRoot>
       <ChakraProvider>
-        <RecoilizeDebugger root={root} />
         <Component {...pageProps} />
       </ChakraProvider>
     </RecoilRoot>
